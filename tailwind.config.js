51708a2495d7e4c3c140ecbf098b6552d2306cd4
GUKWAT <20095319@tafe.wa.edu.au>
2023-12-07 03:07:53 +0800

    Feat: Final Project commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './pages/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
